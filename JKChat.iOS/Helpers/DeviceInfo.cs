f7edb7475d0862054bef82965e44acece909f6bb
entdark <entdark@mail.ru>
2021-12-06 02:06:44 +0300

    iOS: Fixed a possible crash on iOS <11 when trying to access safe area insets

--- JKChat.iOS/Helpers/DeviceInfo.cs
+++ JKChat.iOS/Helpers/DeviceInfo.cs
@@ -12,7 +12,10 @@ using UIKit;
 namespace JKChat.iOS.Helpers {
 	public static class DeviceInfo {
 		public static CGRect ScreenBounds => UIScreen.MainScreen.Bounds;
-		public static UIEdgeInsets SafeAreaInsets => UIApplication.SharedApplication.Windows?[0]?.SafeAreaInsets ?? UIEdgeInsets.Zero;
+		public static UIEdgeInsets SafeAreaInsets => UIDevice.CurrentDevice.CheckSystemVersion(11, 0)
+			&& UIApplication.SharedApplication.Windows?[0]?.SafeAreaInsets is UIEdgeInsets safeAreaInsets
+			? safeAreaInsets
+			: UIEdgeInsets.Zero;
 		public static bool IsPortrait => UIDevice.CurrentDevice.Orientation == UIDeviceOrientation.Portrait || UIDevice.CurrentDevice.Orientation == UIDeviceOrientation.PortraitUpsideDown;
 	}
 }
