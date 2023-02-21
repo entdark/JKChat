a477ff8a05082189594dce3ffac3914fbb1a6664
entdark <entdark@mail.ru>
2023-02-21 05:07:01 +0300

    iOS: initialize application with AppDelegate type rather than name

--- JKChat.iOS/Main.cs
+++ JKChat.iOS/Main.cs
@@ -10,7 +10,7 @@ namespace JKChat.iOS {
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			try {
-				UIApplication.Main(args, null, "AppDelegate");
+				UIApplication.Main(args, null, typeof(AppDelegate));
 			} catch (Exception exception) {
 				Debug.WriteLine(exception);
 			}
